eb2630484816ce19677bb70fadde4d63734e9c44
beum <ubedretdin@gmail.com>
2023-05-26 22:49:29 +0300

    updated function for retrieving threads containing fakecel/truecel

--- src/umit/fakecel_truecel.py
+++ src/umit/fakecel_truecel.py
@@ -1,29 +1,25 @@
 from src.common_basis import *
-import pyarrow as pa
-import pyarrow.compute as pc
-import pandas as pd
-import datetime
-
-import datetime
-
-
-#%%
 
 i = load_incel_parquet()
 df = i.incel_posts
 df2 = df.drop(columns='post_html')
 
-subsample = df2.sample(10000)
-contains_fakecel = subsample[subsample['post_content'].str.contains('fakecel')]
+
+def sample_threads_from_df(dataframe, sample_size: int, query: str):
+    subsample = df.sample(sample_size)
+    contains = subsample[subsample['post_content'].str.contains(query)]
+    # count threads with most mentions of query
+    thread_counts = contains.pivot_table(index=["thread_id"], aggfunc='size').sort_values(ascending=False)
+
+    # retrieve posts from threads with top 10 most mentions of query
+    thread_ids = thread_counts[:10].index
+    queried_posts = df2[df2['thread_id'].isin(thread_ids)]
+    queried_threads = i.incel_threads[i.incel_threads["thread_id"].isin(thread_ids)]
+    queried_df = queried_threads.merge(queried_posts, how="inner", on="thread_id")
+
+    return queried_df, queried_threads, queried_posts,
 
 
-# count threads with most mentions of "fakecel"
-thread_counts = contains_fakecel.pivot_table(index=["thread_id"], aggfunc ='size').sort_values(ascending=False)
-print(thread_counts)
+fakecel_df, fakecel_threads, fakecel_posts = sample_threads_from_df(df2, 10000, "fakecel")
+truecel_df, truecel_threads, truecel_posts = sample_threads_from_df(df2, 10000, "truecel")
 
-# retrieve posts from threads with most mentions of "fakecel"
-thread_ids = thread_counts[:10].index
-
-fakecel_threads = df2[df2['thread_id'].isin(thread_ids)]
-
-fakecel_threads
